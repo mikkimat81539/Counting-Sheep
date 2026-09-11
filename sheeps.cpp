8a5d83e994d3c3dbf6387efa1757347f4cbb67c2
mikkimat81539 <mmatthews81539@gmail.com>
2026-09-11 12:59:22 -0500

    added idle and right state movement based on timing

--- sheeps.cpp
+++ sheeps.cpp
@@ -32,7 +32,7 @@ void sheep_draw(SHEEP &name, float x, float y, Color color){
 }
 
 float sheep_mvmt(SHEEP &object, float dt){
-	object.velocity = 100;
+	object.velocity = 70;
 	object.position.x += object.velocity * dt; 
 
 	return object.position.x;
@@ -49,25 +49,41 @@ int main(){
 	sheep_draw(sheep1, 300, 500, WHITE);
 	sheep_draw(sheep2, 70, 500, GRAY);
 
-
 	sheep1.state = IDLE;
 	sheep2.state = IDLE;
 
-	// vector<SHEEP> sheep_pen = {sheep1, sheep2};
+	int counter = 0;
 
 	// FPS
-	SetTargetFPS(FPS);
+	SetTargetFPS(FPS); // 60 Frames every second
 
 	// GAME LOOP
 	while(!WindowShouldClose()){
 		// Delta Time
 		float dt = GetFrameTime();
 
-		// sheep movement
-		sheep_mvmt(sheep1, dt);
-		// print(sheep1.position.x);
+		// int increment = int(GetTime());
 		
+		print(counter)
+
+		counter++;
+		if (counter >= 120 && sheep1.state == IDLE){
+			sheep1.state = RIGHT;
+		}
+
+
+		if (sheep1.state == RIGHT){
+			sheep_mvmt(sheep1, dt);
+			// print(sheep1.position.x);
+			
+			if (counter == 240 && sheep1.state == RIGHT){
+				sheep1.state = IDLE;
+				counter = 0;
+			}
+		}
+
 		vector<SHEEP> sheep_pen = {sheep1, sheep2};
+
 
 		// DRAW
 		BeginDrawing();
